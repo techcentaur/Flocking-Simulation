f7e040323947fa9aa8950da82da2a76ab1f0e02b
Ankit Solanki <ankit03june@gmail.com>
2018-04-20 12:53:42 +0530

    START: writing javscript code of boid+vector main movement

---
+++ javascript/boid.js
@@ -0,0 +1,23 @@
+/** Main Function : controlling movement of boids */
+function boid(){
+
+}
+/** Adding Prototype: 'update' method to update main class variabeles */
+boid.prototype.update = function() {
+
+}
+
+/** Adding Prototype: 'separation' method to calculate the separation for a boid */ 
+boid.prototype.separation = function(){
+
+}
+
+/** Adding Cohesion: 'cohesion' method to calculate the cohesion for a boid */
+boid.prototype.cohesion = function(){
+
+}
+
+/** Adding Alignment: 'alignment' method to calculate the alignment for a boid */
+boid.prototype.alignment = function(){
+
+}
---
+++ javascript/vector.js
@@ -0,0 +1,45 @@
+/** Simulating Position : representing movement of boids as vectors */
+function vector(){
+
+}
+
+/** Adding Prototype : Calculating magnitude*/
+vector.prototype.magnitude = function(){
+
+}
+
+/** Adding Prototype : Normalising vector*/
+vector.prototype.normalise = function(){
+
+}
+
+
+/** Adding Prototype : Setting limiting values of vector magnitude*/
+vector.prototype.limiting = function(){
+
+}
+
+/** Adding Prototype : Distance between given vector and object vector */
+vector.prototype.distance = function(){
+
+}
+
+/** Prototype: increasing vector co-ordis by a vector inputted as formal parameter */
+vector.prototype.increase = function(){
+
+}
+
+/** Prototype: decreasing vector co-ordis by a vector inputted as formal parameter */
+vector.prototype.decrease = function(){
+	
+}
+
+/** Prototype: scaling up the object vector co-ordis by a vector inputted as formal parameter */
+vector.prototype.upscale = function(){
+	
+}
+
+/** Prototype: scaling down the object vector co-ordis by a vector inputted as formal parameter */
+vector.prototype.downscale = function(){
+	
+}
