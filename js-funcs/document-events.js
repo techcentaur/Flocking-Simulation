85a6e3ca2f586a99efeec5db043d95d7d0585c59
Ankit Solanki <ankit03june@gmail.com>
2018-04-27 13:21:45 +0530

    NEW JS-FUCNTION: document events for mouse controls

---
+++ js-funcs/document-events.js
@@ -0,0 +1,20 @@
+function mouse_move_event(e) {
+	mouse_move_width = e.clientX - window_widthhalf;
+	mouse_move_height = e.clientY - window_heighthalf;
+}
+
+function mouse_touch_start(e) {
+	if (e.touches.length === 1) {
+		e.preventDefault();
+		mouse_move_width = e.touches[0].pageX - window_widthhalf;
+		mouse_move_height = e.touches[0].pageY - window_heighthalf;
+	}
+}
+
+function mouse_touch_move(e) {
+	if ( e.touches.length === 1 ) {
+		e.preventDefault();
+		mouse_move_width = e.touches[0].pageX - window_widthhalf;
+		mouse_move_height = e.touches[0].pageY - window_heighthalf;
+	}
+}
